5ec992cee39199e941855e686b5335c778a12e24
Dwayne Hurst <hurstyd@gmail.com>
2019-10-30 07:56:53 +1000

    Updated 30 Oct 2019

--- hurstygpithemes.sh
+++ hurstygpithemes.sh
@@ -90,7 +90,7 @@ function gui_hurstygpithemes() {
             fi
             ((i++))
         done
-        local cmd=(dialog --default-item "$default" --backtitle "Hursty's ES GPi Themes Installer" --menu "Hursty's ES GPi Themes Installer - (12 Themes as at 29 October 2019)" 22 76 16)
+        local cmd=(dialog --default-item "$default" --backtitle "Hursty's ES GPi Themes Installer" --menu "Hursty's ES GPi Themes Installer - (13 Themes as at 30 October 2019)" 22 76 16)
         local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
         default="$choice"
         [[ -z "$choice" ]] && break
@@ -147,6 +147,7 @@ function gpi_themes() {
         'RetroHursty69 GPi_Circuit'
         'RetroHursty69 GPi_CosmicRise'
         'RetroHursty69 GPi_GPiBoy'
+        'RetroHursty69 GPi_GBColor'		
         'RetroHursty69 GPi_GBGreen'
         'RetroHursty69 GPi_HeyChromeyOfficial'
         'RetroHursty69 GPi_PopBox'
